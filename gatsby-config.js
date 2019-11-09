ce6757f6948d6903013f6c086fa6717a3a588607
Jamie Burnip <jamieburnip@gmail.com>
2019-11-09 00:27:36 +0000

    Fix rendering issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -167,7 +167,7 @@ module.exports = {
                       title
                       cover
                       date
-                      category
+                      categories
                       tags
                     }
                   }
