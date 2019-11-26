fdbfbd568c7fdbf3581dcc63375e70b363b69196
Jamie Burnip <jamieburnip@gmail.com>
2019-11-26 03:10:48 +0000

    Add site mini data

---
+++ data/Projects.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    icon: '🤖',
+    title: 'Jambot (Discord Bot)',
+    source: 'https://github.com/jamieburnip/discord-jambot',
+    description:
+      'A sensible Webpack 4 boilerplate with with Babel, Sass, ESLint, HMR, and prod/dev optimization.',
+  },
+  {
+    icon: '💾',
+    title: 'madebyjamie.dev',
+    source: 'https://github.com/jamieburnip/madebyjamie.dev/',
+    description: 'Personal theme for Gatsby (the source of this website).',
+  },
+];
---
+++ data/Quotes.js
@@ -0,0 +1,18 @@
+export default [
+    {
+      name: 'Kerri Mark Sharp',
+      source: 'Opia Limited',
+      quote: `I first met Jamie when he joined the team at Opia Ltd in September 2016. We were both a part of the same team and worked together on many customer-facing promotional websites for Opia's clients.
+      Throughout my time getting to know Jamie, I found that he is exceptionally knowledgeable about full-stack development, in particular, PHP, Laravel and Vue JS. Jamie worked on many back-end improvements too whilst we worked together, implementing a very impressing queueing system for user notifications.
+      What impressed me most about Jamie is his excitement and enthusiasm about web development and particularly start-up businesses. Jamie is a well-established developer in his field and I do not doubt that he will go far in his career.
+      I'm glad that I had the opportunity to work with Jamie and believe that any employee would be lucky to have him on their team.`,
+    },
+    {
+      name: 'Chris Olsen',
+      source: 'Business Intelligence Controller at Nissan Motor Company',
+      quote: `I've known Jamie in both a personal and professional capacity for several years. Technically Jamie has always had great aptitude for picking up new programming languages, design approaches and frameworks, being able to get the most out of whatever technology he was using and leveraging it into impressive deliverables across the full stack.
+      He excelled at distilling functional business requirements into technical specification and final product. Liaising with stakeholders at all levels throughout the process.
+      Personally, Jamie is amicable, honest and driven with a desire to continuously surpass the level he's expected to perform at.
+      A joy to work with and would make a welcome addition to any web development team where a hard working and innovative mindset is required.`,
+    },
+  ];
