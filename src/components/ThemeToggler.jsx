8b9f537ecca89183302fcd8f37470a810e6647c9
Jamie Burnip <jamieburnip@gmail.com>
2020-06-03 21:09:15 +0100

    Install and use icons

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import useDarkMode from 'use-dark-mode'
-// import styled from '@emotion/styled'
-// import { FiMoon, FiSun } from 'react-icons/fi'
-// import Button from './Button'
+import React from "react";
+import useDarkMode from "use-dark-mode";
+import { FaLightbulb, FaRegLightbulb } from 'react-icons/fa';
 
 const ThemeToggler = () => {
   const darkMode = useDarkMode();
 
-  console.log(darkMode);
-
   return (
-    <button id="dark-mode-toggler" aria-label={ darkMode.value ? 'Switch to light mode' : 'Switch to dark mode' } onClick={darkMode.toggle}>
-        {darkMode.value ? (`Switch to Light Mode`) : (`Switch to Dark Mode`)}
-        {/* {children} */}
+    <button
+      id="dark-mode-toggler"
+      type="button"
+      aria-label={
+        darkMode.value ? "Switch to dark mode" : "Switch to light mode"
+      }
+      onClick={darkMode.toggle}
+    >
+      {darkMode.value ? (
+        <FaRegLightbulb title="Switch to dark mode" />
+      ) : (
+        <FaLightbulb title="Switch to light mode" />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default ThemeToggler
+export default ThemeToggler;
