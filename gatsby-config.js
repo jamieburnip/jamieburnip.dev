d13f339612fe73dad5d21ff3f711a3975fd725a3
Jamie Burnip <jamieburnip@gmail.com>
2020-06-06 15:22:06 +0100

    Fix images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 const urljoin = require("url-join");
 const path = require("path");
+const _ = require("lodash");
 const config = require("./data/SiteConfig");
 
 module.exports = {
-  pathPrefix: config.pathPrefix === "" ? "/" : config.pathPrefix,
+  pathPrefix: config.pathPrefix === `` ? `/` : config.pathPrefix,
   siteMetadata: {
     siteUrl: urljoin(config.siteUrl, config.pathPrefix),
     rssMetadata: {
@@ -19,64 +20,62 @@ module.exports = {
     },
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-lodash",
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-lodash`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "assets",
+        name: `assets`,
         path: `${__dirname}/static/`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "posts",
+        name: `posts`,
         path: `${__dirname}/content/`,
       },
     },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
         gatsbyRemarkPlugins: [
-          "gatsby-remark-prismjs",
-          "gatsby-remark-relative-images",
+          `gatsby-remark-relative-images`,
           {
-            resolve: "gatsby-remark-images",
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 960,
+              withWebp: true,
             },
           },
-          "gatsby-remark-responsive-iframe",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers",
+          `gatsby-remark-responsive-iframe`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-prismjs`,
         ],
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-transformer-remark",
-    },
-    {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: config.googleAnalyticsID,
       },
     },
     {
-      resolve: "gatsby-plugin-nprogress",
+      resolve: `gatsby-plugin-nprogress`,
       options: {
         color: config.themeColor,
       },
     },
-    "gatsby-plugin-catch-links",
-    "gatsby-plugin-use-dark-mode",
-    "gatsby-plugin-twitter",
-    "gatsby-plugin-sitemap",
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-use-dark-mode`,
+    `gatsby-plugin-twitter`,
+    `gatsby-plugin-sitemap`,
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
         name: config.siteTitle,
         short_name: config.siteTitleShort,
@@ -84,29 +83,29 @@ module.exports = {
         start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: "minimal-ui",
+        display: `minimal-ui`,
         icons: [
           {
-            src: "/logos/logo-192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: `/logos/logo-192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
           },
           {
-            src: "/logos/logo-512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: `/logos/logo-512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
           },
         ],
       },
     },
-    "gatsby-plugin-offline",
+    `gatsby-plugin-offline`,
     {
-      resolve: "gatsby-plugin-feed",
+      resolve: `gatsby-plugin-feed`,
       options: {
         setup(ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata;
           ret.allMdx = ref.query.allMdx;
-          ret.generator = "GatsbyJS Advanced Starter";
+          ret.generator = `GatsbyJS Advanced Starter`;
           return ret;
         },
         query: `
