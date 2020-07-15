58431b574e8dc48734e807c179d7b40c5a6f2083
Jamie Burnip <jamieburnip@gmail.com>
2020-07-15 15:21:30 +0100

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,11 +2,11 @@ const config = {
   siteTitle: `I'm Jamie Burnip`,
   siteTitleShort: `I'm Jamie Burnip`,
   siteTitleAlt:
-    'Hello I’m Jamie Burnip and I’m a software developer from Newcastle-upon-tyne.', // Alternative site title for SEO.
-  siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
+    'Hello I’m Jamie Burnip and I’m a software developer from Newcastle-upon-tyne. I’m also the creator of the Heart Coding website and Youtube Channel.', // Alternative site title for SEO.
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://jamieburnip.dev', // Domain of your website without pathPrefix.
   siteEmail: 'me@jamieburnip.dev',
-  siteDescription: `Hi, Hello! I'm Jamie Burnip.`,
+  siteDescription: `Hi, Hello! I'm Jamie Burnip. I’m a software developer from Newcastle-upon-tyne.`,
 
   googleAnalyticsID: 'UA-48020081-6',
 
