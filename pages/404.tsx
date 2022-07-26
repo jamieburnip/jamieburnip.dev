83683cb6bdef44546cee494b17d489896c666a80
Jamie Burnip <jamie.burnip@ticketmaster.co.uk>
2022-07-26 14:44:40 +0100

    Remove all old code

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,13 @@
-import React, { Fragment } from 'react';
-
-import SEO from '../components/seo';
+import React from "react";
+import SEO from "../components/Seo";
 
 const NotFoundPage = () => (
-  <Fragment>
+  <>
     <SEO title="404: Not found" />
+
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Fragment>
+  </>
 );
 
 export default NotFoundPage;
