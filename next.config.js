9bdd8d5403d7022bd468abd4d8cab95a2cb9737f
Jamie Burnip <jamie.burnip@ticketmaster.co.uk>
2022-08-03 12:41:22 +0100

    Build with preact for smaller bundles

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const withPreact = require("next-plugin-preact");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPreact(nextConfig);
